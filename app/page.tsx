63723a9c57adf6f9df262ead1bf57ca7bb0d5ba4
Hope-Have-Happy <3h0925@gmail.com>
2024-03-31 13:28:44 -0400

    12

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { About } from '@/components/sections/About'
 import { Hero } from '@/components/sections/Hero'
 import { Projects } from '@/components/sections/Projects'
-import { Contact } from '@/components/sections/Contact'
+// import { Contact } from '@/components/sections/Contact'
 
 export default function Home() {
     return (
